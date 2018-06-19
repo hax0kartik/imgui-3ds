aceb68e2809b643ec9483382e24c7941ec6a4b05
Pirater12 <pokemonemerald0@gmail.com>
2018-06-19 13:00:30 +0530

    Fix Formatting x2

--- source/main.cpp
+++ source/main.cpp
@@ -14,19 +14,19 @@ int main(int argc, char* argv[])
 	u16 width = 320, height = 240;
 
 	C3D_Tex *tex = (C3D_Tex*)malloc(sizeof(C3D_Tex));
-    static const Tex3DS_SubTexture subt3x = { 512, 256, 0.0f, 1.0f, 1.0f, 0.0f };
+	static const Tex3DS_SubTexture subt3x = { 512, 256, 0.0f, 1.0f, 1.0f, 0.0f };
    
-    C2D_Image image = (C2D_Image){tex, &subt3x };
-    C3D_TexInit(image.tex, 512, 256, GPU_RGBA8);
+	C2D_Image image = (C2D_Image){tex, &subt3x };
+	C3D_TexInit(image.tex, 512, 256, GPU_RGBA8);
    // C3D_TexSetFilter(image.tex, GPU_LINEAR, GPU_LINEAR);
 	C3D_TexSetFilter(image.tex, GPU_LINEAR, GPU_LINEAR);
 	C3D_TexSetWrap(image.tex, GPU_REPEAT, GPU_REPEAT);
 	gfxInitDefault();
 	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
-    C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
-    C2D_Prepare();
+	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+	C2D_Prepare();
 
-    C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
 	consoleInit(GFX_TOP, NULL);
 	printf("Ho!\n");
 	std::vector<uint32_t> pixel_buffer(width * height, 0);
@@ -84,14 +84,14 @@ int main(int argc, char* argv[])
 		paint_imgui(pixel_buffer.data(), width, height, sw_options);
 
 		for (u32 x = 0; x < width; x++)
-        {
-            for (u32 y = 0; y < height; y++)
-            {
-                u32 dstPos = ((((y >> 3) * (512 >> 3) + (x >> 3)) << 6) + ((x & 1) | ((y & 1) << 1) | ((x & 2) << 1) | ((y & 2) << 2) | ((x & 4) << 2) | ((y & 4) << 3))) * 4;
-                u32 srcPos = (y * 320 + x) * 4;
-                memcpy(&((u8*)image.tex->data)[dstPos], &((u8*)pixel_buffer.data())[srcPos], 4);
-            }
-        }
+		{
+    		for (u32 y = 0; y < height; y++)
+        	{
+				u32 dstPos = ((((y >> 3) * (512 >> 3) + (x >> 3)) << 6) + ((x & 1) | ((y & 1) << 1) | ((x & 2) << 1) | ((y & 2) << 2) | ((x & 4) << 2) | ((y & 4) << 3))) * 4;
+				u32 srcPos = (y * 320 + x) * 4;
+				memcpy(&((u8*)image.tex->data)[dstPos], &((u8*)pixel_buffer.data())[srcPos], 4);
+			}
+		}
 		// draw to screen
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		C2D_TargetClear(top, C2D_Color32(32, 38, 100, 0xFF));
